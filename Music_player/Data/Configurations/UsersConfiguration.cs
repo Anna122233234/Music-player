0e071c6d2dc950375688de17fa4c869449ea732b
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-11-22 00:21:38 +0200

    created configuration for Database

---
+++ Music_player/Data/Configurations/UsersConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Music_player.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Music_player.Data.Models;
+
+namespace Music_player.Data.Configurations
+{
+    public class UsersConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(u => u.Name).IsRequired();
+            builder.Property(u => u.Password).IsRequired();
+        }
+}
