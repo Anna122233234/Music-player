b9eaedf462e9faec4bec6d05268dbcc06571a7eb
MelnikAnna <91954998+Anna122233234@users.noreply.github.com>
2022-11-26 01:00:44 +0200

    created IGenericRepository

---
+++ Music_player/Data/Repository/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using Music_player.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Music_player.Data.Repository
+{
+    public interface IGenericRepository
+    {
+        public Task AddUserAsync(User user);
+        public Task<User> GetUserByUsernameAsync(string username);
+        public Task<User> GetUserByIdAsync(int userId);
+        public Task<List<User>> GetAllUserAsync();
+        public Task UpdateUserAsync(User newData);
+        public Task DeleteUserAsync(int userId);
+    }
+}
